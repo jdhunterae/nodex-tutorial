d67277dffafab97c1fc796bc717a4eb79898dc27
Andrew Pomerleau <arpomerleau@gmail.com>
2023-05-09 10:38:44 -0400

    Update server script to connect to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+const mongourl = require('./private/mongo');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Node-API app is running on port ${port}!`);
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog!');
 });
+
+mongoose
+  .connect(mongourl)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(port, () => {
+      console.log(`Node-API app is running on port ${port}!`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Failed to connect to MongoDB: ${error}`);
+  });
 
 // TODO: Video: https://www.youtube.com/watch?v=9OfL9H6AmhQ
 // TODO: Time: 11:29
